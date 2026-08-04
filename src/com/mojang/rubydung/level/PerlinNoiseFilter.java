dc8567cdc32b963c2b66eb67770273bb56f3e55f
DressedAlarm184 <155406555+DressedAlarm184@users.noreply.github.com>
2026-08-03 17:49:23 -0700

    Fix noise generation when width and height aren't equal

--- src/com/mojang/rubydung/level/PerlinNoiseFilter.java
+++ src/com/mojang/rubydung/level/PerlinNoiseFilter.java
@@ -16,53 +16,54 @@ public class PerlinNoiseFilter {
 	}
 
 	public int[] read(int width, int height) {
+		int size = Math.max(width, height);
 		int x;
 		Random random = new Random();
-		int[] tmp = new int[width * height];
+		int[] tmp = new int[size * size];
 		int level = this.levels;
-		int step = width >> level;
+		int step = size >> level;
 		int y = 0;
-		while (y < height) {
+		while (y < size) {
 			x = 0;
-			while (x < width) {
-				tmp[x + y * width] = (random.nextInt(256) - 128) * this.fuzz;
+			while (x < size) {
+				tmp[x + y * size] = (random.nextInt(256) - 128) * this.fuzz;
 				x += step;
 			}
 			y += step;
 		}
-		step = width >> level;
+		step = size >> level;
 		while (step > 1) {
 			int x2;
 			int val = 256 * (step << level);
 			int ss = step / 2;
 			int y2 = 0;
-			while (y2 < height) {
+			while (y2 < size) {
 				x2 = 0;
-				while (x2 < width) {
+				while (x2 < size) {
 					int m;
-					int ul = tmp[(x2 + 0) % width + (y2 + 0) % height * width];
-					int ur = tmp[(x2 + step) % width + (y2 + 0) % height * width];
-					int dl = tmp[(x2 + 0) % width + (y2 + step) % height * width];
-					int dr = tmp[(x2 + step) % width + (y2 + step) % height * width];
-					tmp[x2 + ss + (y2 + ss) * width] = m = (ul + dl + ur + dr) / 4 + random.nextInt(val * 2) - val;
+					int ul = tmp[(x2 + 0) % size + (y2 + 0) % size * size];
+					int ur = tmp[(x2 + step) % size + (y2 + 0) % size * size];
+					int dl = tmp[(x2 + 0) % size + (y2 + step) % size * size];
+					int dr = tmp[(x2 + step) % size + (y2 + step) % size * size];
+					tmp[x2 + ss + (y2 + ss) * size] = m = (ul + dl + ur + dr) / 4 + random.nextInt(val * 2) - val;
 					x2 += step;
 				}
 				y2 += step;
 			}
 			y2 = 0;
-			while (y2 < height) {
+			while (y2 < size) {
 				x2 = 0;
-				while (x2 < width) {
-					int c = tmp[x2 + y2 * width];
-					int r = tmp[(x2 + step) % width + y2 * width];
-					int d = tmp[x2 + (y2 + step) % width * width];
-					int mu = tmp[(x2 + ss & width - 1) + (y2 + ss - step & height - 1) * width];
-					int ml = tmp[(x2 + ss - step & width - 1) + (y2 + ss & height - 1) * width];
-					int m = tmp[(x2 + ss) % width + (y2 + ss) % height * width];
+				while (x2 < size) {
+					int c = tmp[x2 + y2 * size];
+					int r = tmp[(x2 + step) % size + y2 * size];
+					int d = tmp[x2 + (y2 + step) % size * size];
+					int mu = tmp[(x2 + ss & size - 1) + (y2 + ss - step & size - 1) * size];
+					int ml = tmp[(x2 + ss - step & size - 1) + (y2 + ss & size - 1) * size];
+					int m = tmp[(x2 + ss) % size + (y2 + ss) % size * size];
 					int u = (c + r + m + mu) / 4 + random.nextInt(val * 2) - val;
 					int l = (c + d + m + ml) / 4 + random.nextInt(val * 2) - val;
-					tmp[x2 + ss + y2 * width] = u;
-					tmp[x2 + (y2 + ss) * width] = l;
+					tmp[x2 + ss + y2 * size] = u;
+					tmp[x2 + (y2 + ss) * size] = l;
 					x2 += step;
 				}
 				y2 += step;
@@ -74,7 +75,7 @@ public class PerlinNoiseFilter {
 		while (y < height) {
 			x = 0;
 			while (x < width) {
-				result[x + y * width] = tmp[x % width + y % height * width] / 512 + 128;
+				result[x + y * width] = tmp[x % size + y % size * size] / 512 + 128;
 				++x;
 			}
 			++y;
